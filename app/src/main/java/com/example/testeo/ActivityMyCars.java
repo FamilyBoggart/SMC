3e64d816432d0b2297cf8d5b38fa0538daf30c67
Ale <alexanderoga@gmail.com>
2023-04-12 19:46:33 +0200

    Version 0.1.6.2 MyCars tiene los dos botones funcionando

--- app/src/main/java/com/example/testeo/ActivityMyCars.java
+++ app/src/main/java/com/example/testeo/ActivityMyCars.java
@@ -1,8 +1,17 @@
 package com.example.testeo;
 
+import static com.example.testeo.R.id.mycars_btn_add_car;
+import static com.example.testeo.R.id.mycars_btn_back;
+import static com.example.testeo.R.id.ui_btn_my_cars;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testeo.Objects.Usuario;
 
 public class ActivityMyCars extends AppCompatActivity {
 
@@ -10,5 +19,28 @@ public class ActivityMyCars extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cars);
+
+        Intent intent  = getIntent();
+        Usuario user = (Usuario) intent.getSerializableExtra("objUser");
+
+        Button backButton = findViewById(mycars_btn_back);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityMyCars.this, ActivityUI.class);
+                intent.putExtra("objUser",user);
+                startActivity(intent);
+            }
+        });
+
+
+        Button addCarButton = findViewById(mycars_btn_add_car);
+        addCarButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityMyCars.this, ActivityAddCar.class);
+                intent.putExtra("objUser",user);
+                startActivity(intent);
+            }
+        });
+
     }
 }
