5f81a39731432f4e982786552b72e91c62dce4f9
Ale <alexanderoga@gmail.com>
2023-04-12 19:36:58 +0200

    Version 0.1.6.0 ActivityUI funciona bien (faltan los componentes) y lleva a MyCars

---
+++ app/src/main/java/com/example/testeo/ActivityMyCars.java
@@ -0,0 +1,14 @@
+package com.example.testeo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityMyCars extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cars);
+    }
+}
--- app/src/main/java/com/example/testeo/ActivityUI.java
+++ app/src/main/java/com/example/testeo/ActivityUI.java
@@ -1,8 +1,12 @@
 package com.example.testeo;
 
+
+import static com.example.testeo.R.id.ui_btn_my_cars;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,34 +17,30 @@ public class ActivityUI extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-
-        Intent intent=getIntent();
-        Usuario user = (Usuario) intent.getSerializableExtra("objUser");
-        System.out.println(user.getNombre());
-        System.out.println(user.getEmail());
-
-        setName(user.getNombre());
-
         super.onCreate(savedInstanceState);
+        Intent intent=getIntent();
         setContentView(R.layout.activity_ui);
 
+        //Establecer nombre
+        Usuario user = (Usuario) intent.getSerializableExtra("objUser");
+        TextView nombre = findViewById(R.id.txt_name_yours);
+        nombre.setText(user.getNombre());
+
+        //Button a mis coches
+        Button loginButton = findViewById(ui_btn_my_cars);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityUI.this, ActivityMyCars.class);
+                intent.putExtra("objUser",user);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
-    private int userID(){
-        Intent intent = getIntent();
-        int userId = intent.getIntExtra("id",-1);
-        return userId;
 
-    }
-    private void setName(String name){
 
-       TextView nombre = findViewById(R.id.txt_name_yours);
-    try{
-        nombre.setText((CharSequence) name);}
-    catch(Exception e){
-        Log.e("Error: ",e.getMessage());
-    }
-    }
 
 
 
