fbecd87779f489f7278db182cf75310f59fca297
Ale <alexanderoga@gmail.com>
2023-04-14 11:45:04 +0200

    Version 0.1.6.9 Primeras pruebas con la interfaz de MyCars

--- app/src/main/java/com/example/testeo/ActivityMyCars.java
+++ app/src/main/java/com/example/testeo/ActivityMyCars.java
@@ -1,5 +1,6 @@
 package com.example.testeo;
 
+import static com.example.testeo.R.id.carContainer;
 import static com.example.testeo.R.id.mycars_btn_add_car;
 import static com.example.testeo.R.id.mycars_btn_back;
 import static com.example.testeo.R.id.ui_btn_my_cars;
@@ -8,10 +9,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testeo.Objects.Coche;
 import com.example.testeo.Objects.Usuario;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ActivityMyCars extends AppCompatActivity {
 
@@ -23,6 +29,17 @@ public class ActivityMyCars extends AppCompatActivity {
         Intent intent  = getIntent();
         Usuario user = (Usuario) intent.getSerializableExtra("objUser");
 
+        // Contenedor de coches
+        LinearLayout container = findViewById(carContainer);
+        List<Coche> cars = user.getCoches();
+        if (cars.isEmpty()){}
+        else{
+            container.setVisibility(View.VISIBLE);
+            for (Coche coche:cars) {
+                
+            }
+        }
+        // Botones
         Button backButton = findViewById(mycars_btn_back);
         backButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
