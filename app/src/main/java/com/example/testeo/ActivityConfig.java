15d6268699006768014f82d39895e296b145c6da
Ale <alexanderoga@gmail.com>
2023-04-20 15:58:46 +0200

    0.10.1 Activity config se modifica su diseño y ahora se accede a los datos del ususario, el boton aun no funciona

--- app/src/main/java/com/example/testeo/ActivityConfig.java
+++ app/src/main/java/com/example/testeo/ActivityConfig.java
@@ -2,14 +2,17 @@ package com.example.testeo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.testeo.Objects.Usuario;
+
+import org.w3c.dom.Text;
 
 public class ActivityConfig extends AppCompatActivity {
     @Override
@@ -20,6 +23,8 @@ public class ActivityConfig extends AppCompatActivity {
         Intent intent = getIntent();
         Usuario user = (Usuario) intent.getSerializableExtra("objUser");
 
+        showData(user);
+
         Button backButton = findViewById(R.id.config_btn_back_to_ui);
         backButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
@@ -29,6 +34,16 @@ public class ActivityConfig extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public void showData(Usuario user){
+
+        TextView userName = findViewById(R.id.config_edit_txt_name);
+        userName.setText(user.getNombre());
+
+        TextView userMail = findViewById(R.id.config_edit_txt_mail);
+        userMail.setText(user.getEmail());
 
     }
 }
